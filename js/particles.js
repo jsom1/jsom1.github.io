ef0ff4b3d9b0a786e3a59667cc84288d09a43c10
jsom1 <42206612+jsom1@users.noreply.github.com>
2020-07-21 10:35:51 +0200

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -47,21 +47,6 @@ particlesJS("particles-js", {
       "color": "#e8dad5",
       "opacity": 0.5,
       "width": 1
-    },
-  },
-    "move": {
-    "enable": true,
-    "speed": 1,
-    "direction": "none",
-    "random": true,
-    "straight": false,
-    "out_mode": "bounce",
-    "bounce": false,
-    "attract": {
-      "enable": false,
-      "rotateX": 600,
-      "rotateY": 600
-      }
     }
   },
   "interactivity": {
